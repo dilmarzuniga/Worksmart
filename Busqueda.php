5c79a7a71d286b7b63cdafc0c47113123c8d7698
chestofwonders <alex.bayona94@gmail.com>
2012-08-19 16:31:17 -0600

    Arreglado el error de la busqueda

--- Busqueda.php
+++ Busqueda.php
@@ -1,4 +1,4 @@
-
+<?php session_start(); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -140,7 +140,7 @@ $(document).ready(function(){
     <!--cuenta-->
 <div class="left_content"><?php 
 				  
-						session_start();
+
 						
 						require_once 'PHPPaging.lib.php';
 						include_once 'admin/conexion.php';
