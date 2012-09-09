e8c4e3f799ab3dad7781688b531aee9a4627c044
William David Parras <william.parras.mendez@gmail.com>
2012-09-09 07:46:39 -0600

    Problema de placeholder solucionado, volviendo a value

--- modcurri.php
+++ modcurri.php
@@ -102,82 +102,82 @@ $totEmp = mysql_num_rows($resEmp);
           <tr>
             <td width="119">Nombres</td>
             <td width="171"><label>
-              <input name="names" type="text" id="names" placeholder="<?php echo $nombre; ?>" />
+              <input name="names" type="text" id="names" value="<?php echo $nombre; ?>" />
             </label></td>
             <td width="121">Sexo</td>
             <td width="194"><label>
               <select name="sex" size="1" id="sex">
-                <option placeholder="<?php echo $sexo; ?>" selected="selected"><?php echo $sexo; ?></option>
-                <option placeholder="Masculino">Masculino</option>
-                <option placeholder="Femenino">Femenino</option>
+                <option value="<?php echo $sexo; ?>" selected="selected"><?php echo $sexo; ?></option>
+                <option value="Masculino">Masculino</option>
+                <option value="Femenino">Femenino</option>
               </select>
             </label></td>
             <td width="178">Pa&iacute;s
-              <input name="country" type="text" id="country" placeholder="<?php echo $pais; ?>"/></td>
+              <input name="country" type="text" id="country" value="<?php echo $pais; ?>"/></td>
           </tr>
           <tr>
             <td>Apellidos</td>
-            <td><input name="last" type="text" id="last" placeholder="<?php echo $apellidos; ?>"/></td>
+            <td><input name="last" type="text" id="last" value="<?php echo $apellidos; ?>"/></td>
             <td>Estudios B&aacute;scos </td>
             <td><textarea name="basics" id="basics"><?php echo $basicos; ?></textarea></td>
             <td>Mensaje Personal</td>
           </tr>
           <tr>
             <td>Fecha Nacimiento </td>
-            <td><input name="bird" type="text" id="bird" placeholder="<?php echo $nacimiento; ?>"/></td>
+            <td><input name="bird" type="text" id="bird" value="<?php echo $nacimiento; ?>"/></td>
             <td>Bachillerato</td>
             <td><textarea name="bach" id="bach" ><?php echo $bachillerato; ?></textarea></td>
             <td><textarea name="msj" id="msj"><?php echo $mensaje; ?></textarea></td>
           </tr>
           <tr>
             <td>Tel&eacute;fono</td>
-            <td><input name="tel" type="text" id="tel" placeholder="<?php echo $telefono; ?>"/></td>
+            <td><input name="tel" type="text" id="tel" value="<?php echo $telefono; ?>"/></td>
             <td>Estudios Superiores </td>
             <td><textarea name="high" id="high" ><?php echo $superiores; ?></textarea></td>
             <td>&nbsp;</td>
           </tr>
           <tr>
             <td>DUI</td>
-            <td><input name="iud" type="text" id="iud" placeholder="<?php echo $dui; ?>"/></td>
+            <td><input name="iud" type="text" id="iud" value="<?php echo $dui; ?>"/></td>
             <td>Experiencia Laboral </td>
-            <td><input name="exp" type="text" id="exp" placeholder="<?php echo $experiencia; ?>"/></td>
+            <td><input name="exp" type="text" id="exp" value="<?php echo $experiencia; ?>"/></td>
             <td><label>
-              <input name="Guardar" type="submit" id="Guardar" placeholder="Guardar" />
+              <input name="Guardar" type="submit" id="Guardar" value="Guardar" />
             </label></td>
           </tr>
           <tr>
             <td>NIT</td>
-            <td><input name="tin" type="text" id="tin" placeholder="<?php echo $nit; ?>"/></td>
+            <td><input name="tin" type="text" id="tin" value="<?php echo $nit; ?>"/></td>
             <td>Profesi&oacute;n</td>
-            <td><input name="prof" type="text" id="prof" placeholder="<?php echo $profesion; ?>"/></td>
+            <td><input name="prof" type="text" id="prof" value="<?php echo $profesion; ?>"/></td>
             <td>&nbsp;</td>
           </tr>
           <tr>
             <td>Direccion</td>
-            <td><input name="dir" type="text" id="dir" placeholder="<?php echo $direccion; ?>"/></td>
+            <td><input name="dir" type="text" id="dir" value="<?php echo $direccion; ?>"/></td>
             <td>Referencia Personal </td>
-            <td><input name="ref" type="text" id="ref" placeholder="<?php echo $referencia; ?>"/></td>
+            <td><input name="ref" type="text" id="ref" value="<?php echo $referencia; ?>"/></td>
             <td>&nbsp;</td>
           </tr>
           <tr>
             <td>Celular</td>
-            <td><input name="cel" type="text" id="cel" placeholder="<?php echo $celular; ?>"/></td>
+            <td><input name="cel" type="text" id="cel" value="<?php echo $celular; ?>"/></td>
             <td>Profesion Referencia </td>
-            <td><input name="prof_ref" type="text" id="prof_ref" placeholder="<?php echo $prof_ref; ?>"/></td>
+            <td><input name="prof_ref" type="text" id="prof_ref" value="<?php echo $prof_ref; ?>"/></td>
             <td>&nbsp;</td>
           </tr>
           <tr>
             <td>Estado Civil </td>
-            <td><input name="marital" type="text" id="marital" placeholder="<?php echo $civil; ?>"/></td>
+            <td><input name="marital" type="text" id="marital" value="<?php echo $civil; ?>"/></td>
             <td>Telefono Referencia </td>
-            <td><input name="tel_ref" type="text" id="tel_ref" placeholder="<?php echo $tel_ref; ?>"/></td>
+            <td><input name="tel_ref" type="text" id="tel_ref" value="<?php echo $tel_ref; ?>"/></td>
             <td>&nbsp;</td>
           </tr>
           <tr>
             <td>Correo Electr&oacute;nico </td>
-            <td><input name="mail" type="text" id="mail" placeholder="<?php echo $correo; ?>"/></td>
+            <td><input name="mail" type="text" id="mail" value="<?php echo $correo; ?>"/></td>
             <td>Correo Referencia </td>
-            <td><input name="mail_ref" type="text" id="mail_ref" placeholder="<?php echo $correo_ref; ?>"/></td>
+            <td><input name="mail_ref" type="text" id="mail_ref" value="<?php echo $correo_ref; ?>"/></td>
             <td>&nbsp;</td>
           </tr>
           <tr>
