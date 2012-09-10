175b38d7200528a14c43a424e788cfba7a492d2a
William David Parras <william.parras.mendez@gmail.com>
2012-09-09 22:22:05 -0600

    nueva interfaz

--- modcurri.php
+++ modcurri.php
@@ -17,21 +17,13 @@ include_once 'admin/conexion.php';
 <link rel="stylesheet" href="css/home.css" />
 
 <body>
-<table width="1381" height="1009" border="1">
-  <tr>
-    <td width="212" height="167">&nbsp;</td>
-    <td width="1106">&nbsp;</td>
-    <td width="41">&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="21">Bienvenido <?php
+Bienvenido <?php
 	if( strlen($user)==0){
 	echo("<script>alert('Inicie Sesion para acceder al menu de usuario');</script>");
 	header("location:Index.php?mostrar=2");
 	}	
 		echo("<font size=5>$user</font>");
-		?> <a href="login.php">Cerrar Sesion</a> </td>
-    <td rowspan="2">
+		?> 
 	  <?php
 	if($accion=='alert'){
 	echo("<script>alert('Datos Actualizados');</script>");
@@ -71,7 +63,7 @@ $totEmp = mysql_num_rows($resEmp);
 	}
 	else
 	{
-					$id="No data";	
+				$id="No data";	
 				$nombre = "No data";
 				$apellidos = "No data";
 				$nacimiento="No data";
@@ -97,118 +89,124 @@ $totEmp = mysql_num_rows($resEmp);
 	}	
 	include("procesos.php");
 	?>
+		  <ul id="options">
+    <li><a href="home.php">Inicio</a></li>
+    <li><a href="modcurri.php">Mi Curriculum</a></li>
+    <li><a href="procesos.php?id=bye&id=<?php echo $id_user?>">Eliminar Mi Cuenta</a></li>
+    <li><a href="login.php">Cerrar Sesion</a></li>
+    </ul>
+    <p>
+    <br />
+    <br />
+    </p>
+    <h3> Datos de Curriculum</h3>
       <form id="form1" name="form1" method="post" action="procesos.php?accion=save&id=<?php echo $user?>">
-        <table width="817" height="255" border="1">
-          <tr>
-            <td width="119">Nombres</td>
-            <td width="171"><label>
+      <p>
+      		<br />
+            Nombres
+            <br />
+            <label>
               <input name="names" type="text" id="names" value="<?php echo $nombre; ?>" />
-            </label></td>
-            <td width="121">Sexo</td>
-            <td width="194"><label>
+            </label>
+            <br />
+            Sexo
+            <br />
+            <label>
               <select name="sex" size="1" id="sex">
                 <option value="<?php echo $sexo; ?>" selected="selected"><?php echo $sexo; ?></option>
                 <option value="Masculino">Masculino</option>
                 <option value="Femenino">Femenino</option>
               </select>
-            </label></td>
-            <td width="178">Pa&iacute;s
-              <input name="country" type="text" id="country" value="<?php echo $pais; ?>"/></td>
-          </tr>
-          <tr>
-            <td>Apellidos</td>
-            <td><input name="last" type="text" id="last" value="<?php echo $apellidos; ?>"/></td>
-            <td>Estudios B&aacute;scos </td>
-            <td><textarea name="basics" id="basics"><?php echo $basicos; ?></textarea></td>
-            <td>Mensaje Personal</td>
-          </tr>
-          <tr>
-            <td>Fecha Nacimiento </td>
-            <td><input name="bird" type="text" id="bird" value="<?php echo $nacimiento; ?>"/></td>
-            <td>Bachillerato</td>
-            <td><textarea name="bach" id="bach" ><?php echo $bachillerato; ?></textarea></td>
-            <td><textarea name="msj" id="msj"><?php echo $mensaje; ?></textarea></td>
-          </tr>
-          <tr>
-            <td>Tel&eacute;fono</td>
-            <td><input name="tel" type="text" id="tel" value="<?php echo $telefono; ?>"/></td>
-            <td>Estudios Superiores </td>
-            <td><textarea name="high" id="high" ><?php echo $superiores; ?></textarea></td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>DUI</td>
-            <td><input name="iud" type="text" id="iud" value="<?php echo $dui; ?>"/></td>
-            <td>Experiencia Laboral </td>
-            <td><input name="exp" type="text" id="exp" value="<?php echo $experiencia; ?>"/></td>
-            <td><label>
+            </label>
+            <br />
+            <br />
+            Pa&iacute;s
+            <br />
+            <input name="country" type="text" id="country" value="<?php echo $pais; ?>"/>
+            <br />
+            Apellidos
+            <br />
+            <input name="last" type="text" id="last" value="<?php echo $apellidos; ?>"/>
+            <br />
+            Estudios B&aacute;scos
+            <br />
+            <textarea name="basics" id="basics"><?php echo $basicos; ?></textarea>
+            <br />
+            <br />
+            Mensaje Personal
+            <br />
+            <textarea name="msj" id="msj"><?php echo $mensaje; ?></textarea>
+            <br />
+            <br />
+            Fecha Nacimiento
+            <br />
+            <input name="bird" type="text" id="bird" value="<?php echo $nacimiento; ?>"/>
+            <br />
+            Bachillerato
+            <br />
+            <textarea name="bach" id="bach" ><?php echo $bachillerato; ?></textarea>
+            <br />
+            Tel&eacute;fono
+            <br />
+            <input name="tel" type="text" id="tel" value="<?php echo $telefono; ?>"/>
+            <br />
+            Estudios Superiores
+            <br />
+            <textarea name="high" id="high" ><?php echo $superiores; ?></textarea>
+            <br />
+            DUI
+            <br />
+            <input name="iud" type="text" id="iud" value="<?php echo $dui; ?>"/>
+            <br />
+            Experiencia Laboral
+            <br />
+            <input name="exp" type="text" id="exp" value="<?php echo $experiencia; ?>"/>
+            <br />
+            NIT
+            <br />
+            <input name="tin" type="text" id="tin" value="<?php echo $nit; ?>"/>
+            <br />
+            Profesi&oacute;n
+            <br />
+            <input name="prof" type="text" id="prof" value="<?php echo $profesion; ?>"/>
+            <br />
+            Direccion
+            <br />
+            <input name="dir" type="text" id="dir" value="<?php echo $direccion; ?>"/>
+            <br />
+            Referencia Personal
+            <br />
+            <input name="ref" type="text" id="ref" value="<?php echo $referencia; ?>"/>
+            <br />
+            Celular
+            <br />
+            <input name="cel" type="text" id="cel" value="<?php echo $celular; ?>"/>
+            <br />
+            Profesion Referencia 
+            <br />
+            <input name="prof_ref" type="text" id="prof_ref" value="<?php echo $prof_ref; ?>"/>
+            <br />
+            Estado Civil
+            <br />
+            <input name="marital" type="text" id="marital" value="<?php echo $civil; ?>"/>
+            <br />
+            Telefono Referencia
+            <br />
+            <input name="tel_ref" type="text" id="tel_ref" value="<?php echo $tel_ref; ?>"/>
+            <br />
+            Correo Electr&oacute;nico
+            <br />
+            <input name="mail" type="text" id="mail" value="<?php echo $correo; ?>"/>
+            <br />
+            Correo Referencia
+            <br />
+            <input name="mail_ref" type="text" id="mail_ref" value="<?php echo $correo_ref; ?>"/>
+            <br />
+            <br />
+            <label>
               <input name="Guardar" type="submit" id="Guardar" value="Guardar" />
-            </label></td>
-          </tr>
-          <tr>
-            <td>NIT</td>
-            <td><input name="tin" type="text" id="tin" value="<?php echo $nit; ?>"/></td>
-            <td>Profesi&oacute;n</td>
-            <td><input name="prof" type="text" id="prof" value="<?php echo $profesion; ?>"/></td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>Direccion</td>
-            <td><input name="dir" type="text" id="dir" value="<?php echo $direccion; ?>"/></td>
-            <td>Referencia Personal </td>
-            <td><input name="ref" type="text" id="ref" value="<?php echo $referencia; ?>"/></td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>Celular</td>
-            <td><input name="cel" type="text" id="cel" value="<?php echo $celular; ?>"/></td>
-            <td>Profesion Referencia </td>
-            <td><input name="prof_ref" type="text" id="prof_ref" value="<?php echo $prof_ref; ?>"/></td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>Estado Civil </td>
-            <td><input name="marital" type="text" id="marital" value="<?php echo $civil; ?>"/></td>
-            <td>Telefono Referencia </td>
-            <td><input name="tel_ref" type="text" id="tel_ref" value="<?php echo $tel_ref; ?>"/></td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>Correo Electr&oacute;nico </td>
-            <td><input name="mail" type="text" id="mail" value="<?php echo $correo; ?>"/></td>
-            <td>Correo Referencia </td>
-            <td><input name="mail_ref" type="text" id="mail_ref" value="<?php echo $correo_ref; ?>"/></td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-          <tr>
-            <td>&nbsp;</td>
-            <td><label></label></td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-            <td>&nbsp;</td>
-          </tr>
-        </table>
-    </form></td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td height="344"><p><a href="home.php">Inicio</a></p>
-      <p><a href="modcurri.php">Mi Curriculum</a></p>
-    <p><a href="procesos.php?id=bye&id=<?php echo $id_user?>">Eliminar Mi Cuenta</a>   </p></td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-</table>
+            </label>
+	</p>
+    </form>
 </body>
 </html>
